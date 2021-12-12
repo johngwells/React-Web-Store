dbc7062a287f370a23da55039994229de98ed4e8
John Wells <molekewl2017@gmail.com>
2021-12-11 18:05:07 -0800

    change some naming in the logic

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -3,31 +3,30 @@ import { useReducer } from "react";
 import CartContext from "./cart-context";
 
 const initialState = {
-  cartItems: [],
-}
+  cartItems: []
+};
 
 function reducer(state, action) {
-  if (action.type === 'ADD') {
-    console.log(state)
+  if (action.type === "ADD") {
+    console.log(state);
 
     return {
       ...state,
-      cartItems: [...state.cartItems, {...action.payload}],
-    }
+      cartItems: [...state.cartItems, { ...action.payload, quantity: 1}]
+    };
   }
-  if (action.type === 'REMOVE') {
+  if (action.type === "REMOVE") {
     return {
       ...state,
       cartItems: [...state.cartItems.filter(item => item.id !== action.payload.id)]
-      // count: state.count < 0 ? 0 : state.count--
-    }
+    };
   }
 
-  if (action.type === 'RESET') {
+  if (action.type === "RESET") {
     return {
       ...state,
       cartItems: []
-    }
+    };
   }
 
   return state;
@@ -36,24 +35,23 @@ function reducer(state, action) {
 const CartProvider = (props) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const addItemToCartHandler = (item) => {
-    dispatch({type: 'ADD', payload: item});
-  }
+  const handleAddItemToCart = (item) => {
+    dispatch({ type: "ADD", payload: item });
+  };
 
-  const removeItemToCartHandler = (item) => {
-    dispatch({type: 'REMOVE', payload: item});
-  }
+  const handleRemoveItemToCart = (item) => {
+    dispatch({ type: "REMOVE", payload: item });
+  };
 
-  const resetCartHandler = (item) => {
-    dispatch({type: 'RESET', payload: item});
-  }
-  
-  
+  const resetCart = (item) => {
+    dispatch({ type: "RESET", payload: item });
+  };
+
   const cartContext = {
     cart: state,
-    addItem: addItemToCartHandler,
-    removeItem: removeItemToCartHandler,
-    reset: resetCartHandler
+    addItem: handleAddItemToCart,
+    removeItem: handleRemoveItemToCart,
+    reset: resetCart
   };
 
   return (
